4f9ffd64a0af0da125b2fd34c3c13d8b98e5e1c1
diana <diana@DESKTOP-892CUT5>
2023-01-26 19:51:43 -0500

    trabajo 0302

---
+++ src/main/java/com/mycompany/deber0203/Principal.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.deber0203;
+
+/**
+ *
+ * @author diana
+ */
+public class Principal {
+    public static void main(String[] args) {
+
+        var ventana = new Deber0302a("");
+        var ventana1 = new Deber0302b("");
+        var ventana2 = new Deber0302c(" ");
+        var ventana3 = new Deber0302d("");
+    }
+}
